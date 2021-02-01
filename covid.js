4f010d36aa833968598f754f9715e53df263b821
Hans Barche <hansbarche@web.de>
2021-02-01 23:00:44 +0100

    New feature: custom time ranges 	* you can now select a custom range to show in the charts

--- covid.js
+++ covid.js
@@ -44,9 +44,9 @@ const drawData = (canvas, labels, data) => {
                     type: 'time',
                     time: {
                         displayFormats: {
-                            second: 'MMM d',
-                            minute: 'MMM d',
-                            hour: 'MMM d',
+                            second: 'MMM D',
+                            minute: 'MMM D',
+                            hour: 'MMM D',
                         }
                     }
                 }]
@@ -58,6 +58,8 @@ const drawData = (canvas, labels, data) => {
 }
 
 let chartCases,chartIncidence;
+let startDateElement, endDateElement;
+let dropdown;
 
 const renderSite = async (days) => {
     chartCases = drawData(
@@ -74,8 +76,8 @@ const renderSite = async (days) => {
 }
 
 const updateSite = async (days) => {
-    const cases = caseData.slice(caseData.length - days, caseData.length);
-    const incidences = incidenceData.slice(incidenceData.length - days, incidenceData.length);
+    const cases = caseData.slice(days == 0 ? 0 : caseData.length - days, caseData.length);
+    const incidences = incidenceData.slice(days == 0 ? 0 : incidenceData.length - days, incidenceData.length);
     
     chartCases.data.labels.length = 0;
     chartCases.data.labels = Array.from(cases, elements => elements.date);
@@ -92,30 +94,99 @@ const updateSite = async (days) => {
     chartIncidence.update();
 }
 
+const updateSiteWithRange = (start, end) => {
+    const cases = caseData.filter(element => moment(element.date).isAfter(start) && moment(element.date).isBefore(end));
+    const incidences = incidenceData.filter(element => moment(element.date).isAfter(start) && moment(element.date).isBefore(end));
+
+    chartCases.data.labels.length = 0;
+    chartCases.data.labels = Array.from(cases, elements => elements.date);
+    chartCases.data.datasets[0].data.length = 0;
+    chartCases.data.datasets[0].data = Array.from(cases, elements => elements.cases);
+
+    chartCases.update();
+
+    chartIncidence.data.labels.length = 0;
+    chartIncidence.data.labels = Array.from(incidences, elements => elements.date);
+    chartIncidence.data.datasets[0].data.length = 0;
+    chartIncidence.data.datasets[0].data = Array.from(incidences, elements => elements.weekIncidence);
+
+    chartIncidence.update();
+}
+
+const dateChanged = () => {
+    const start = startDateElement.value;
+    const end = endDateElement.value;
+    dropdown.value = '0';
+    console.log(`Start ${start} Ende ${end}`);
+
+    updateSiteWithRange(start, end);
+}
+
+const dateRangeChanged = () => {
+    switch(dropdown.value){
+        case '0':
+            break;
+        case '1':
+            updateSite(7);
+            startDateElement.value = moment(caseData[caseData.length-1].date).subtract(6, 'days').format('YYYY-MM-DD');
+            break;
+        case '2':
+            updateSite(30);
+            startDateElement.value = moment(caseData[caseData.length-1].date).subtract(29, 'days').format('YYYY-MM-DD');
+            break;
+        case '3':
+            updateSite(92);
+            startDateElement.value = moment(caseData[caseData.length-1].date).subtract(91, 'days').format('YYYY-MM-DD');
+            break;
+        case '4':
+            updateSite(182);
+            startDateElement.value = moment(caseData[caseData.length-1].date).subtract(181, 'days').format('YYYY-MM-DD');
+            break;
+        case '5':
+            updateSite(0);
+            startDateElement.value = caseData[0].date.slice(0, 10);
+            break;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async (event) => {
     caseData = await getDistricData('14511', 'cases');
     incidenceData = await getDistricData('14511', 'incidence');
 
-    const dropdown = document.getElementById("daysSelector");
-    dropdown.onchange = () => {
-        switch(dropdown.value){
-            case '0':
-                renderSite(0);
-                break;
-            case '1':
-                updateSite(7);
-                break;
-            case '2':
-                updateSite(30)
-                break;
-            case '3':
-                updateSite(92);
-                break;
-            case '4':
-                updateSite(182);
-                break;
-        }
-    };
+    startDateElement = document.getElementById('start');
+    startDateElement.setAttribute('min', caseData[0].date.slice(0, 10));
+    startDateElement.setAttribute('max', caseData[caseData.length-1].date.slice(0, 10));
+    startDateElement.setAttribute('value', caseData[0].date.slice(0, 10));
+    startDateElement.addEventListener('change', dateChanged)
+    
+    endDateElement = document.getElementById('end');
+    endDateElement.setAttribute('min', caseData[0].date.slice(0, 10));
+    endDateElement.setAttribute('max', caseData[caseData.length-1].date.slice(0, 10));
+    endDateElement.setAttribute('value', caseData[caseData.length-1].date.slice(0, 10));
+    endDateElement.addEventListener('change', dateChanged);
+
+
+    dropdown = document.getElementById("daysSelector");
+    dropdown.addEventListener('change', dateRangeChanged);
+    //  = () => {
+    //     switch(dropdown.value){
+    //         case '0':
+    //             updateSite(0);
+    //             break;
+    //         case '1':
+    //             updateSite(7);
+    //             break;
+    //         case '2':
+    //             updateSite(30)
+    //             break;
+    //         case '3':
+    //             updateSite(92);
+    //             break;
+    //         case '4':
+    //             updateSite(182);
+    //             break;
+    //     }
+    // };
 
     renderSite(0);
 });
